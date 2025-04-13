c2aad4dd5e79de626f10adc8ed1ac267aea116aa
imilwkk <milena.subanova@alatoo.edu.kg>
2025-04-13 13:31:22 +0600

    added src directory w main, user, room & reservation(finished user)

---
+++ src/User.java
@@ -0,0 +1,25 @@
+public class User {
+    private String name;
+    private String email;
+    private String phone;
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+    public String getName() { return name; }
+    public void setName(String name) {this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) {this.email = email; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) {this.phone = phone; }
+    @Override
+    public String toString() {
+        return name + " , " + email + " , " + phone;
+    }
+    public static User fromString(String data) {
+        String [] parts = data.split(" , ");
+        return new User(parts[0], parts[1], parts[2]);
+    }
+
+}
